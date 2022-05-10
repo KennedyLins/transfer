a2e0dd85088a8ee4ed7a52a42c86e33f80beb3e2
Kennedy Lins <kennedy.lins@gmail.com>
2022-05-10 15:37:36 -0300

    relationships

---
+++ database/migrations/2022_05_10_180948_create_transfers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransfersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('source');
+            $table->string('destiny');
+            $table->string('date');
+            $table->string('time');
+            $table->integer('people');
+            $table->integer('observation');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transfers');
+    }
+}
